30bfde0749c1c70b4bd71ffbdd0e91175b14c728
NicolasRCaro <nicolas.ramirezc@uniagustiniana.edu.co>
2026-04-03 00:14:25 +0000

    Tercer Commit

--- App.js
+++ App.js
@@ -1,15 +1,54 @@
 import React, { useState } from 'react';
-import Lista from './lista';
+import { View, Button, StyleSheet } from 'react-native';
+import Lista from './Lista';
 import Original from './Original';
 
-function App() {
+function Navegacion() {
   const [pantalla, setPantalla] = useState('Lista');
-  if (pantalla === 'Lista') {
-        return <Lista irAOriginal={() => setPantalla('Original')} />;
-  }
-  if (pantalla === 'Original') {
-    return <Original volverALista={() => setPantalla('Lista')} />;
-  }
-  return null;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.nav}>
+        <Button title="Lista" onPress={() => setPantalla('Lista')} />
+        <Button title="Original" onPress={() => setPantalla('Original')} />
+      </View>
+      <View style={styles.content}>
+        {pantalla === 'Lista' && <Lista/>}
+        {pantalla === 'Original' && <Original/>}
+      </View>
+    </View>
+  );
 }
-export default App;
+export default Navegacion;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#111",
+    padding: 20
+  },
+  title: {
+    color: "white",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 20
+  },
+  nav: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 20
+  },
+  button: {
+    backgroundColor: "#000000",
+    padding: 10,
+    alignItems: "center",
+    borderRadius: 8,
+    minWidth: 100
+  },
+  activeButton: {
+    backgroundColor: "#666"
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold"
+  }
+});
